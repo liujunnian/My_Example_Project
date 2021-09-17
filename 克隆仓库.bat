20d8410db3a614523556ab90374b285aeeb59664
liujunnian <1150602882@qq.com>
2021-09-17 21:06:10 +0800

    新增脚本示例文件

---
+++ 克隆仓库.bat
@@ -0,0 +1,7 @@
+::@echo on
+::git checkout master
+::git clone git@gitee.com:makeblock-test-dev_enterprise/snippets.git
+::git clone git@gitee.com:makeblock-test-dev_enterprise/sample_repository.git
+::pause>nul
+
+
